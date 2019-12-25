2ddfc4c03c4775720a3d186442f522b9f83e70c8
wandsu <wandersonbezerralima@gmail.com>
2019-12-24 22:32:52 -0200

    Solution added

---
+++ securiy.py
@@ -0,0 +1,13 @@
+casino_floor = str(input())
+
+def verify(string):
+	danger = bool(False)
+	for i in string:
+		if(not danger and (i == '$' or i == 'T')):
+			danger = True
+		elif(danger and (i == '$' or i == 'T')):
+			return 'ALARM'
+		elif(danger and i == 'G'):
+			danger = False
+	return 'quiet'
+print(verfy(casino_floor)) 
